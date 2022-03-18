702e8303749bf3626588ad85371f07431940206f
d.dolotkazin <d.dolotkazin@redmadrobot.com>
2022-03-18 18:20:37 +0300

    minor fixes

---
+++ internal/routers/handlers.go
@@ -0,0 +1,59 @@
+package routers
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"shortener/pkg/logging"
+	"strings"
+)
+
+type HandlePostShortenReqBody struct {
+	Url string `json:"url"`
+}
+
+func (h *Handler) HandlePostShorten(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	if req.Method == http.MethodPost && req.URL.Path == "/" {
+		var requestBody HandlePostShortenReqBody
+		decoder := json.NewDecoder(req.Body)
+		err := decoder.Decode(&requestBody)
+		if err != nil {
+			logging.EasyLogError("routers", "failed to parse request body for method",
+				" POST /", err)
+			http.Error(w, "Error! Failed to parse request body!", http.StatusBadRequest)
+		}
+
+		res, err := h.services.UrlService.TransformUrl(context.TODO(), requestBody.Url)
+		if err != nil {
+			logging.EasyLogError("routers", "failed to get short url for method",
+				" POST /", err)
+			http.Error(w, "Error! Failed to generate short url!", http.StatusBadRequest)
+		}
+		result, err := json.Marshal(res)
+		if err != nil {
+			logging.EasyLogError("routers", "failed to parse response body for method",
+				" POST /", err)
+			http.Error(w, "Error! Failed to return short url!", http.StatusBadRequest)
+		}
+
+		w.WriteHeader(201)
+		w.Write(result)
+		return
+	} else if req.Method == http.MethodGet && req.URL.Path == "/" {
+		path := strings.Trim(req.URL.Path, "/")
+		res, err := h.services.UrlService.ReTransformUrl(context.TODO(), path)
+		if err != nil {
+			logging.EasyLogError("routers", "failed to get response body for method",
+				" GET /:id", err)
+			http.Error(w, "Error! Failed to return full url!", http.StatusBadRequest)
+		}
+		w.Header().Add("Location", res)
+		w.WriteHeader(307)
+		return
+	} else {
+		logging.EasyLogInfo("routers", "request denied - unhallowed method for: ",
+			" POST /")
+		http.Error(w, "Error! Method not allowed", http.StatusBadRequest)
+	}
+}
